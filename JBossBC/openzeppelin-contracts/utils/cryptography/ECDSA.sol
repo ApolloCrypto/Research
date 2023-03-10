b49ff053e30f2360dc78d05e3ef97df51195d2d4
xiyang <jbossbc@gmail.com>
2023-03-10 21:09:40 +0800

    ECDSA

--- JBossBC/openzeppelin-contracts/utils/cryptography/ECDSA.sol
+++ JBossBC/openzeppelin-contracts/utils/cryptography/ECDSA.sol
@@ -60,4 +60,33 @@ library ECDSA{
         }
         return(signer,RecoverError.NoError);
     }
+    function recover(bytes32 hash,uint8 v,bytes32 r,bytes32 s)internal pure returns(address){
+        (address recovered,RecoverError error)=tryRecover(hash,v,r,s);
+        _throwError(error);
+        return recovered;
+    }
+    function toEthSignedMessageHash(bytes32 hash)internal pure returns(bytes32 message){
+              assembly{
+                mstore(0x00, "\x19Ethereum Signed Message:\n32")
+                mstore(0x1c,hash)
+                message:=keccak256(0x00,0x3c)
+              }
+    }
+    function toEthSignedMessageHash(bytes memory s)internal pure returns (bytes32){
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n", Strings.toString(s.length), s));
+    }
+    function toTypedDataHash(bytes32 domainSeparator,bytes32 structHash)internal pure returns (bytes32 data){
+        assembly{
+            let ptr:=mload(0x40)
+            mstore(ptr,"\x19\x01")
+            mstore(add(ptr,0x02),domainSeparator)
+            mstore(add(ptr,0x22),structHash)
+            data:=keccak256(ptr,0x42)
+
+        }
+    }
+        function toDataWithIntendeValidatorHash(address validator,bytes memory data)internal pure returns(bytes32){
+            return keccak256(abi.encodePacked("\x19\x00",validator,data));
+        }
+    
 }
