0d4420fa51f3263f6be99c3e0d8232f8d8861dee
OLLIEo <1951717459@qq.com>
2023-03-16 22:21:46 +0800

    web3

---
+++ OLLIEo/code/web3/contract.js
@@ -0,0 +1,17 @@
+const contractAddress="0x73daCD4ab88528eeC50E814d5d46A84f1e3Cc7A7";
+const contractABI=[
+	{
+		"inputs": [],
+		"name": "greet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	}
+];
+ 
